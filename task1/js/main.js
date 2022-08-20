d563bb1fa8c07977e03a2b391a4fbd6900068393
ibaasalman <ibaasalman0@gmail.com>
2022-08-20 18:25:48 +0300

    add edit functionality

--- task1/js/main.js
+++ task1/js/main.js
@@ -1,6 +1,7 @@
 items = [];
 id = 0;
 const cont = document.querySelector('.cont');
+const btn = document.querySelector('.addbutton');
 
 const savetolocal = ()=>{
 
@@ -16,6 +17,92 @@ const deletHandler = (elm) => {
         savetolocal();
     }
 }
+
+const btnaddhandler = () => {
+    let title = document.getElementById("title").value;
+    let price = document.getElementById("price").value;
+    let img = document.getElementById("img").value;
+    let des = document.getElementById("des").value;
+    let btext = document.getElementById("btext").value;
+    let bcolor = document.getElementById("bcolor").value;
+    
+    myitem = {
+        id: ++id,
+        title,
+        price,
+        img,
+        des,
+        btext,
+        bcolor
+    }
+    items.push(myitem);
+    addItem(myitem);
+    savetolocal();
+    fillform({
+        id : '',
+        title : '',
+        price : '',
+        img : '',
+        des : '' ,
+        btext : '' ,
+        bcolor :''
+    })}
+
+const btnedithandler = () => {
+    let oldid = document.getElementById("hideid").value;
+    let title = document.getElementById("title").value;
+    let price = document.getElementById("price").value;
+    let img = document.getElementById("img").value;
+    let des = document.getElementById("des").value;
+    let btext = document.getElementById("btext").value;
+    let bcolor = document.getElementById("bcolor").value;
+    
+    myitem = {
+        id: oldid,
+        title,
+        price,
+        img,
+        des,
+        btext,
+        bcolor
+    }
+    let index = items.findIndex((item)=>{
+        return item.id == oldid
+    })
+    items[index] = myitem;
+    savetolocal();
+    fillform({
+        id:'',
+        title : '',
+        price : '',
+        img : '',
+        des : '' ,
+        btext : '' ,
+        bcolor :''
+    })
+    refresh();
+    btn.innerHTML = 'Add product';
+    btn.mode = "";
+}
+const editHandler = (elm) => {
+        editID = elm.target.parentNode.firstChild.value;
+        e_item = items.filter((prod)=>{
+            return prod.id == editID;
+        })[0];
+        fillform(e_item);
+        btn.innerHTML = 'Edit';
+        btn.mode = "edit";
+}
+
+const fillform = (elm) => {
+    document.getElementById("hideid").value = elm.id;
+    document.getElementById("title").value = elm.title;
+    document.getElementById("price").value = elm.price;
+    document.getElementById("img").value = elm.img;
+    document.getElementById("des").value = elm.des;
+    document.getElementById("btext").value = elm.btext;
+    document.getElementById("bcolor").value = elm.bcolor;
+} 
 const addItem = (element)=>{
     item=  document.createElement('div');
         item.classList.add('item');
@@ -49,13 +136,14 @@ const addItem = (element)=>{
         t5 = document.createTextNode("Edit");
         editbutton.style.background = "var(--primary)";
         editbutton.appendChild(t5);
+        editbutton.addEventListener('click',editHandler);
     
         deletbutton = document.createElement("button");
         deletbutton.style.background = "var(--red)";
         t6 = document.createTextNode("Delet");
         deletbutton.classList.add('btndelet');
         deletbutton.appendChild(t6);
-        deletbutton.addEventListener('click',deletHandler)
+        deletbutton.addEventListener('click',deletHandler);
     
        item.appendChild(hiddenId);
        item.appendChild(badge);
@@ -76,28 +164,16 @@ const refresh = ()=>{
     }
 
 
-const btn = document.querySelector('.addbutton');
-btn.addEventListener('click', () => {
-    let title = document.getElementById("title").value;
-    let price = document.getElementById("price").value;
-    let img = document.getElementById("img").value;
-    let des = document.getElementById("des").value;
-    let btext = document.getElementById("btext").value;
-    let bcolor = document.getElementById("bcolor").value;
-    
-    myitem = {
-        id: ++id,
-        title,
-        price,
-        img,
-        des,
-        btext,
-        bcolor
-    }
-    items.push(myitem);
-    addItem(myitem);
-    savetolocal();
-})
+
+btn.addEventListener('click', (elem)=>{
+if(elem.target.mode == "edit"){
+btnedithandler(elem);
+}
+else{
+    btnaddhandler(elem);
+}
+}
+)
 
 
 const edit = document.querySelectorAll('.btnedit');
